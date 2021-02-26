5a732735f4d069352d98122c13589b3fa71d84d3
vlad-zakiev <58986642+vlad-zakiev@users.noreply.github.com>
2021-02-26 09:39:10 +0500

    created my folder

---
+++ cypress/integration/wp-admin/login.spec.js
@@ -0,0 +1,8 @@
+import msinstall from '../chunks/install-ms';
+import login from '../chunks/login-wp-admin';
+
+describe('Upload files', () => {
+    login('admin', '1', )
+        //msinstall();
+
+})
