b9e3c8f4eb32440d862c6f259b668ae2a4fa30f9
vlad-zakiev <vzakiev32@gmail.com>
2021-03-05 22:36:38 +0500

    commentet some

--- cypress/integration/chunks/zoom-settings.js
+++ cypress/integration/chunks/zoom-settings.js
@@ -8,17 +8,20 @@ export default function ZoomSettings() {
         cy.get('#toplevel_page_stm_zoom > .wp-submenu > :nth-child(8) > a').click()
     })
     it('Clearing key forms', () => {
+        //clearing the form
         cy.get('#tab_1-api_key').clear()
         cy.get('#tab_1-api_secret').clear()
     })
     it('Input zoom keys', () => {
-        //cy.get('#tab_1-api_key').clear()
-        cy.get('#tab_1-api_key').type('vl76_OO2Rku9r0ixz6CrIQ')
-            //cy.get('#tab_1-api_secret').clear()
-        cy.get('#tab_1-api_secret').type('wfPBVV7gFOVeZE3w1gpE23o0i235rzcIt1t3')
+
+        //inputing zoom api key
+        cy.get('#tab_1-api_key').type('vl76_OO2Rku9r0ixz6CrIQ' /*<< API KEY*/ )
+            //inputing zoom secret key
+        cy.get('#tab_1-api_secret').type('wfPBVV7gFOVeZE3w1gpE23o0i235rzcIt1t3' /*<< SECRET KEY*/ )
     })
 
     it('Saving settings', () => {
+        //saving settings
         cy.get('.button > span').should('contain', 'Save Settings').click()
     })
 
